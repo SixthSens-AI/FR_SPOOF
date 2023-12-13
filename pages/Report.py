c35da779fe67bf752e41a8566049aa4818dd108c
SixthSens-AI <152181108+SixthSens-AI@users.noreply.github.com>
2023-12-13 12:01:46 +0530

    Update Report.py

--- pages/Report.py
+++ pages/Report.py
@@ -24,12 +24,7 @@ import face_rec
 # # st.set_page_config(page_title="Reporting")
 # st.subheader("Report")
 
-# extract data from redis
-name = 'attendance:logs'
-def load_logs(name, end=-1):
-    data = face_rec.r.lrange(name, 0, end)
-    data = [x.decode('utf-8') for x in data]
-    return data
+
 
 st.header("Registered People")
 
