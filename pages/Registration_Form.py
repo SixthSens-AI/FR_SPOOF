f83c5c54954f3f9138f839a044907133c2047881
SixthSens-AI <152181108+SixthSens-AI@users.noreply.github.com>
2023-12-13 11:58:43 +0530

    Update Registration_Form.py

--- pages/Registration_Form.py
+++ pages/Registration_Form.py
@@ -1,4 +1,24 @@
 import streamlit as st
+st.set_page_config(
+    page_title="Person Recognition System | SixthSens AI",
+    page_icon=":smiley:",
+    layout="wide",
+    initial_sidebar_state="expanded",
+)
+
+hide_streamlit_style = """
+            <style>
+            .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
+            .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
+            .viewerBadge_text__1JaDK {display: none;}
+            MainMenu {visibility: hidden;}
+            header { visibility: hidden; }
+            footer {visibility: hidden;}
+            #GithubIcon {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+
 import face_rec
 import numpy as np
 from streamlit_webrtc import webrtc_streamer, VideoHTMLAttributes
